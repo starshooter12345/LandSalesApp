3388f1e255aae8157bb936fde2afd87bd6c13f2a
sJanith <88935216+sJanith@users.noreply.github.com>
2021-09-26 12:37:00 +0530

    Shamila 8

--- app/src/main/java/com/project/landmanagementcode/rentview.java
+++ app/src/main/java/com/project/landmanagementcode/rentview.java
@@ -1,14 +1,72 @@
 package com.project.landmanagementcode;
 
-import android.os.Bundle;
-
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
-public class rentview extends AppCompatActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class rentmain extends AppCompatActivity {
+    FloatingActionButton fab;
+    ActionBar actionBar;
+    RecyclerView mRecyclerView;
+    DatabaseHelper databaseHelper;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.rent_view);
+        setContentView(R.layout.activity_main);
+
+        actionBar = getSupportActionBar();
+        actionBar.setTitle("Rental properties");
+
+        mRecyclerView = findViewById(R.id.recyclerView);
+        databaseHelper = new DatabaseHelper(this);
+
+        showRecord();
+
+        fab=findViewById(R.id.addFabButton);
+
+        //added to navigate to feedback page
+        Button navtofeed = (Button) findViewById(R.id.feedButtonn);
+        navtofeed.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                Intent intent = new Intent(MainActivity.this,Feedback.class);
+                startActivity(intent);
+            }
+        });
+
+        fab.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                //create a new activity
+                Intent intent = new Intent(MainActivity.this, AddRecordActivity.class);
+                intent.putExtra("editMode",false);
+                startActivity(intent);
+            }
+        });
     }
+    private void showRecord(){
+        Adapter adapter = new Adapter(MainActivity.this, databaseHelper.getAllData(Constants.C_ADD_TIMESTAMP + " DESC"));
+        //because last added record shows on top
+        mRecyclerView.setAdapter(adapter);
+
+    }
+    protected void onResume(){
+        super.onResume();
+        showRecord();
+    }
+    //kills all start activities
+  /*  public boolean onKeyDown(int keyCode, KeyEvent event){
+        if(keyCode == event.KEYCODE_BACK){
+            moveTaskToBack(true);
+
+        }
+        return super.onKeyDown(keyCode,event);
+
+    }*/
 }
